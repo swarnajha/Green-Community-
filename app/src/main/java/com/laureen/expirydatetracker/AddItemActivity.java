62ea7f98bd2c74afd5ee2b5c89d45f087b94b966
Laureen Fernandes <laureenfernandes212@gmail.com>
2021-07-10 16:59:40 +0530

    Added "Add Item" Page

--- app/src/main/java/com/laureen/expirydatetracker/AddItemActivity.java
+++ app/src/main/java/com/laureen/expirydatetracker/AddItemActivity.java
@@ -4,48 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-public class ItemActivity extends AppCompatActivity {
+public class AddItemActivity extends AppCompatActivity {
     TextView title;
-    ImageView icon;
-    FloatingActionButton fab;
+    ImageView icon, add_date;
+    Button add;
+    EditText date1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item);
+        setContentView(R.layout.activity_add_item);
 
         title = findViewById(R.id.page_title);
-        fab = findViewById(R.id.fab_item);
         icon = findViewById(R.id.toolbar_icon);
+        add = findViewById(R.id.add_cat);
+        date1 = findViewById(R.id.expiry_date1);
+        add_date = findViewById(R.id.add_date);
 
         //Change the page title
-        title = findViewById(R.id.page_title);
-        title.setText(R.string.item_list_title);
+        title.setText(R.string.add_category);
 
         //Change the top left icon to a back button
-        icon = findViewById(R.id.toolbar_icon);
         icon.setImageResource(R.drawable.left_arrow);
         icon.setContentDescription("Go Back");
 
         icon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ItemActivity.this, MainActivity.class);
+                Intent intent = new Intent(AddItemActivity.this, ItemActivity.class);
                 startActivity(intent);
             }
         });
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ItemActivity.this, AddItemActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
--- app/src/main/java/com/laureen/expirydatetracker/MainActivity.java
+++ app/src/main/java/com/laureen/expirydatetracker/MainActivity.java
@@ -8,25 +8,35 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
     TextView title;
     ImageView icon;
+    FloatingActionButton fab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Change the page title
         title = findViewById(R.id.page_title);
+        fab = findViewById(R.id.fab_cat);
+        icon = findViewById(R.id.toolbar_icon);
+
+        //Change the page title
         title.setText(R.string.category_list_title);
 
         //Change the top left icon to a back button
-        icon = findViewById(R.id.toolbar_icon);
         icon.setImageResource(R.drawable.toolbar_logo);
         icon.setContentDescription("Logo");
-    }
-    public void gotoB(View v) {
-        Intent intent = new Intent(MainActivity.this, AddCategoryActivity.class);
-        startActivity(intent);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddCategoryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
